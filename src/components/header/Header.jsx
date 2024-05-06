5bda8d4a8196eecaa27575f02dca14fa9a3b0239
premabarua2310 <premab1053@gmail.com>
2024-05-06 11:35:11 +0600

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './header.scss';
+
+import logo from '../../assets/tmovie.png';
+
+const Header = () => {
+    return (
+        <div>Header</div>
+    )
+}
+
+export default Header;
